9a05b9666f9936516da89031833018499e795961
Arun <yarunkumar136@gmail.com>
2023-10-23 22:41:30 +0530

    addConfig

---
+++ src/Components/AddConfig.jsx
@@ -0,0 +1,172 @@
+import React from 'react'
+import { Button } from 'primereact/button';
+import { InputText } from 'primereact/inputtext';
+import { MultiSelect } from 'primereact/multiselect';
+import { useState } from "react"
+import "./AddConfig.css"
+const AddConfig = () => {
+
+  const [selectedCities, setSelectedCities] = useState(null);
+
+  const cities = [
+    { name: 'New York', code: 'NY' },
+    { name: 'Rome', code: 'RM' },
+    { name: 'London', code: 'LDN' },
+    { name: 'Istanbul', code: 'IST' },
+    { name: 'Paris', code: 'PRS' }
+  ];
+  return (
+    <>
+      <div className="from-main">
+        <div className="from-body">
+          <div className="form-header">
+            <div className="right">
+              <p>Create</p>
+              <h1>New User</h1>
+
+            </div>
+            <div className="left">
+
+              <Button label="Close" icon="pi pi-times" iconPos="right" severity="success" className='header-btn' />
+
+            </div>
+          </div>
+
+
+          <div className="inpute-section">
+            <div className='inpute-type'>
+              <span>Employee ID <span style={{ color: "red" }}>*</span></span>
+            </div>
+            <div className='inpute-hover'>
+              <InputText placeholder='Enter Employee id' className='inpute' />
+
+            </div>
+            <div className='inpute-type'>
+              <span>Employee Name <span style={{ color: "red" }}>*</span></span>
+
+            </div>
+            <div className='inpute-hover'>
+              <InputText placeholder='Enter Name' className='inpute' />
+
+            </div>
+            <div className='inpute-type'>
+              <span>Employee Email <span style={{ color: "red" }}>*</span></span>
+
+            </div>
+            <div className='inpute-hover'>
+              <InputText placeholder='Enter Email' type='email' className='inpute' />
+
+            </div>
+
+
+            <div className='inpute-type'>
+              <span>Employee Level <span style={{ color: "red" }}>*</span></span>
+
+            </div>
+            <div>
+
+              <MultiSelect value={selectedCities} onChange={(e) => setSelectedCities(e.value)} options={cities} optionLabel="name"
+                placeholder="Select " maxSelectedLabels={3} className="inpute" />
+
+            </div>
+            <div className='inpute-type'>
+              <span>Department <span style={{ color: "red" }}>*</span></span>
+
+            </div>
+            <div>
+
+              <MultiSelect value={selectedCities} onChange={(e) => setSelectedCities(e.value)} options={cities} optionLabel="name"
+                placeholder="Select " maxSelectedLabels={3} className="inpute" />
+
+            </div>
+
+            <div className='inpute-type'>
+              <span>Document Access Type  <span style={{ color: "red" }}>*</span></span>
+
+            </div>
+            <div>
+
+              <MultiSelect value={selectedCities} onChange={(e) => setSelectedCities(e.value)} options={cities} optionLabel="name"
+                placeholder="Select " maxSelectedLabels={3} className="inpute" />
+
+            </div>
+            <div className='inpute-type'>
+              <span>Country <span style={{ color: "red" }}>*</span></span>
+
+            </div>
+            <div>
+
+              <MultiSelect value={selectedCities} onChange={(e) => setSelectedCities(e.value)} options={cities} optionLabel="name"
+                placeholder="Select " maxSelectedLabels={3} className="inpute" />
+
+            </div>
+            <div className='inpute-type'>
+              <span>Vendor <span style={{ color: "red" }}>*</span></span>
+
+            </div>
+            <div>
+
+              <MultiSelect value={selectedCities} onChange={(e) => setSelectedCities(e.value)} options={cities} optionLabel="name"
+                placeholder="Select " maxSelectedLabels={3} className="inpute" />
+
+            </div>
+            <div className='inpute-type'>
+              <span>Product Group <span style={{ color: "red" }}>*</span></span>
+
+            </div>
+            <div>
+
+              <MultiSelect value={selectedCities} onChange={(e) => setSelectedCities(e.value)} options={cities} optionLabel="name"
+                placeholder="Select " maxSelectedLabels={3} className="inpute" />
+
+            </div>
+            <div className='inpute-type'>
+              <span>End User Type</span>
+
+            </div>
+            <div>
+
+              <MultiSelect value={selectedCities} onChange={(e) => setSelectedCities(e.value)} options={cities} optionLabel="name"
+                placeholder="Select " maxSelectedLabels={3} className="inpute" />
+
+            </div>
+            <div className='inpute-type'>
+              <span>Approval Rights <span style={{ color: "red" }}>*</span></span>
+
+            </div>
+            <div>
+
+              <MultiSelect value={selectedCities} onChange={(e) => setSelectedCities(e.value)} options={cities} optionLabel="name"
+                placeholder="Select " maxSelectedLabels={3} className="inpute" />
+
+            </div>
+            <div className='inpute-type'>
+              <span>Approval Amount <span style={{ color: "red", }}>*</span></span>
+
+            </div>
+            <div>
+
+              <MultiSelect value={selectedCities} onChange={(e) => setSelectedCities(e.value)} options={cities} optionLabel="name"
+                placeholder="Select " maxSelectedLabels={3} className="inpute" />
+
+            </div>
+
+          </div>
+          <div className="footer">
+            <div className="btn-footer">
+
+              <Button label="Cancel" severity="secondary" text raised className='footer-btn' style={{ backgroundColor: "white" }} />
+              <Button label='Create' severity="success" className='footer-btn' />
+
+
+            </div>
+
+          </div>
+
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default AddConfig
--- src/index.js
+++ src/index.js
@@ -3,12 +3,20 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-
+import "primereact/resources/themes/bootstrap4-light-blue/theme.css"; 
+import "primereact/resources/primereact.min.css";                  //core css
+import "primeicons/primeicons.css"; 
+        
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    
+      
+      <App />
+    
   </React.StrictMode>
+
+
 );
 
 // If you want to start measuring performance in your app, pass a function
